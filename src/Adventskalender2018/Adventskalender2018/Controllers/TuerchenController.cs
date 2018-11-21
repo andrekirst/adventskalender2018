a8447b00440207a8f237ddfb2e625c5ed5dbfd56
André Kirst <github@andrekirst.de>
2018-11-21 18:43:33 +0100

    Erster Entwurf

---
+++ src/Adventskalender2018/Adventskalender2018/Controllers/TuerchenController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Adventskalender2018.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adventskalender2018.Controllers
+{
+    public class TuerchenController : Controller
+    {
+        public IActionResult Tuer(int tag)
+        {
+            return View(new TuerModel
+            {
+                Tag = tag
+            });
+        }
+    }
+}
---
+++ src/Adventskalender2018/Adventskalender2018/Models/TuerModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adventskalender2018.Models
+{
+    public class TuerModel
+    {
+        public int Tag { get; set; }
+    }
+}

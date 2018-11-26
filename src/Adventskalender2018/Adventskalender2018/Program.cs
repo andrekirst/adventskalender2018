d64efbf6fe7ad70cdb5480995303c90749469fa2
André Kirst <github@andrekirst.de>
2018-11-26 20:28:25 +0100

    Entfernung unnötigem Code

--- src/Adventskalender2018/Adventskalender2018/Program.cs
+++ src/Adventskalender2018/Adventskalender2018/Program.cs
@@ -15,19 +15,12 @@ namespace Adventskalender2018
     {
         public static async Task Main(string[] args)
         {
-            //CreateWebHostBuilder(args: args).Build().Run();
             await new WebHostBuilder()
                 .UseKestrel()
-                //.ConfigureServices(configureServices: services => services.AddAutofac())
                 .UseContentRoot(contentRoot: Directory.GetCurrentDirectory())
-                //.UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build()
                 .RunAsync();
         }
-
-        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args: args)
-        //        .UseStartup<Startup>();
     }
 }

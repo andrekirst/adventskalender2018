29887aaf450ef33a9a60768f862f5c154eb836b2
André Kirst <github@andrekirst.de>
2018-11-30 23:46:51 +0100

    Abschlusstests

--- src/Adventskalender2018/Adventskalender2018/Implementations/Infrastructure/WorldClockApiDateTimeProvider.cs
+++ src/Adventskalender2018/Adventskalender2018/Implementations/Infrastructure/WorldClockApiDateTimeProvider.cs
@@ -7,23 +7,18 @@ namespace Adventskalender2018.Implementations.Infrastructure
 {
     public class WorldClockApiDateTimeProvider : IDateTimeProvider
     {
-        private readonly WebClient _webClient;
-
-        public WorldClockApiDateTimeProvider()
-        {
-            _webClient = new WebClient();
-        }
-
         public DateTime Today
         {
             get
             {
                 try
                 {
-                    string jsonData = _webClient.DownloadString(address: "http://worldclockapi.com/api/json/cet/now");
-                    var x = JObject.Parse(json: jsonData);
-                    var y = x.Value<DateTime>(key: "currentDateTime");
-                    return y;
+                    using (WebClient webClient = new WebClient())
+                    {
+                        string jsonData = webClient.DownloadString(address: "http://worldclockapi.com/api/json/cet/now");
+                        JObject parsedJObject = JObject.Parse(json: jsonData);
+                        return parsedJObject.Value<DateTime>(key: "currentDateTime");
+                    }
                 }
                 catch (Exception)
                 {
